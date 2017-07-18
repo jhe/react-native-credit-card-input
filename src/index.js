9d5e9f7e0218059702d4c024183743cc2239c286
Jeffrey J Rozic <jeffreyjrozic@Jeffreys-MacBook-Pro.local>
2017-07-18 15:26:24 -0700

    export more files so a project's component can create it's own credit card entry form

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import connectToState from "./connectToState";
 import CCF from "./CreditCardInput";
 import LiteCCF from "./LiteCreditCardInput";
 import CV from "./CardView";
+import CCI from './CCInput';
 
 export const CreditCardInput = connectToState(CCF);
 export const LiteCreditCardInput = connectToState(LiteCCF);
 export const CardView = CV;
+export const connectToCreditCardState = connectToState;
+export const CreditCard = CV;
+export const CCInput = CCI;
